0d133395cbbd93771badb01d2ee7c6f57c522def
Mike332149 <saqrmike332@gmail.com>
2025-08-14 14:01:14 +0300

    input  exampel

---
+++ lesson_2.py
@@ -0,0 +1,33 @@
+# inmutable
+# str String
+# int Integer
+# float 5.3
+# bool Boolean
+
+# mutable
+# list List
+# tuple
+# dict Dictionary
+# set
+
+
+#some =[1]
+#some2 =[1]
+#print(id(some),id(some2))
+
+#a = [1]
+#b = [1]
+#c = b
+#print(id(a), id(b), id(c))
+
+#greetings = ["Hello", "I'm Vova"]
+#print(greetings[0])
+#print(greetings[1])
+#print(divmod(16,2))
+#print(divmod(55,2))
+#print(help(divmod))
+input_number = int (input("Enter number: "))
+input_number2 = int(input("Enter number2: "))
+
+print(type(input_number))
+print(input_number + input_number2)
