549cb0ca2a087a04299d20105029d51db66fd9b8
Mike332149 <saqrmike332@gmail.com>
2025-09-19 15:56:56 +0300

    Програма переносить останній елемент списку з кінця на початок списку. Послідовність інших елементів не змінюється. Порожній список або з одним елемнтом не має змінюватись.

---
+++ DZ_Lesson_3.2.py
@@ -0,0 +1,10 @@
+# Програма переносить останній елемент списку з кінця на початок списку. Послідовність інших елементів не змінюється.
+# Порожній список або з одним елемнтом не має змінюватись.
+
+list_original = [3,5,7,9,11,13]
+x = len(list_original)
+
+if len(list_original) != 0 and 1:
+    list_for_client =list_original[::-x]+list_original [:-1]
+print(list_original)
+print(list_for_client)
